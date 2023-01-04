cd2713916189ff1a26041547c60092b1838eac85
Harshit <gharshit89@gmail.com>
2023-01-05 03:39:22 +0530

    update

--- basics/src/App.js
+++ basics/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Restaurant from './component/basics/Restaurant';
 import UseState from './component/Hooks/useState';
 import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
 
 const App = () => {
   return (
     <>
    {/* <Restaurant /> */}
    {/* <UseState /> */}
-   <UseEffect />
+   {/* <UseEffect /> */}
+   <UseReducer />
     </>
   )
 }
